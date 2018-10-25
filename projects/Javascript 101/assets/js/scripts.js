208c6c1caa2e78fd7f6277e5e9bc53db6aa80b30
Adam Jonsson <adamjons@lycksele.se>
2018-10-25 14:03:56 +0200

    init commit

---
+++ projects/Javascript 101/assets/js/scripts.js
@@ -0,0 +1,59 @@
+var name = Adam 
+var age = 18
+
+if (age <18){
+    console.log("Shas med dig");
+} else {
+    console.log ("Men hejsan du");
+}
+
+var secretNumber = 7;
+var userInput == Number (prompt("gissa ett tal mellan 1 och 10"));
+if (UserInput == secretNumber){
+    document.WriteIn("Gz");
+}
+else if (userInput == 6 || userInput == 8){
+    document.WriteIn("Burn it does")
+}
+else if (userInput > secretNumber){
+    document.WriteIn("Its over .... !")
+}
+else {
+    document.WriteIn 
+}
+
+
+
+
+// Deklarera en variabel
+var secretNumber = 7;
+// User input - Ta emot data
+
+var userInput = Number(prompt("Gissa ett numer mellan 1-1000 d-_ -b")); // Detta returneras som en sträng
+
+// Läsa av villkoret - Läser av user input 
+if(userInput == secretNumber){
+    document.writeln("Grattis du har vunnit en ny mobil <br>");
+}
+else if(userInput == 6 || userInput == ) {
+    document.writeln("Det bränns!");
+}
+else if(userInput > secretNumber){
+        document.writeln("Du gissade för högt! testa igen...")
+}
+else{
+   document.writeln("DU har förlorat 1000kr från kontot" ); 
+}
+
+var age = Number(prompt("Daddys ålder"));
+
+if(age < 0){
+    //Du är inte född ännu min pojke
+    document.writeln("You are already dead!");
+}
+else if(age % 2 != 0  ){
+    document.writeln("Du är udda Människa" );
+}
+else{
+    document.writeln("Du är jämn");
+}

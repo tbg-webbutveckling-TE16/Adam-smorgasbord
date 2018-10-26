d8f8466d94d9d4d05ef836a513d648d653a309c5
Adam Jonsson <adamjons@lycksele.se>
2018-10-26 11:18:41 +0200

    Added some code to functions

---
+++ README.md
@@ -0,0 +1,9 @@
+#Välkommen till mitt smörgåsbord i Webbutveckling 2
+
+##Generell info
+Miit namn är Adam och jag studerar på Teknikprogrammet i Lycksele
+
+### att göra
+* lära mig JavaScript
+* while()
+* for()
---
+++ assets/js/functions.js
@@ -0,0 +1,16 @@
+function greet(person1, person2, person3, person4) {
+    console.log("Hej. " + person1 + "!");
+    console.log("Hej. " + person2 + "!");
+    console.log("Hej. " + person3 + "!");
+    console.log("Hej. " + person4 + "!");
+}
+
+greet("Dennis", "Hej", "Adam", "Viktor");
+
+function squared(x) {
+    return x * x;
+}
+console.log("4 squared is: " + squared(4));
+
+var result = squared(10);
+console.log(result);

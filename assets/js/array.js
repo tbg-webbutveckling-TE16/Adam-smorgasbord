a81ce258d0aa30787cd39f28fbc03ba6189f4b23
Adderino <jonssonadam1@gmail.com>
2018-11-16 11:17:04 +0100

    Mer arbete med arrays etc.

--- assets/js/array.js
+++ assets/js/array.js
@@ -4,3 +4,28 @@ var friend3 = "Paul";
 var friend4 = "Yeet";
 
 var friends = ["Lind", "Holm", "Är", "Finsk"];
+
+console.log(friends[2] + " <3 " + friends[3]);
+function newName() {
+    friends[2] = "Göran";
+    friends[3] = true;
+}
+newName();
+
+console.log (friends[2] + "<3" friends[3] );
+
+var colors = []; 
+var colors = new Array();
+var randomArray = ["String", 31, true, false];
+console.log(randomArray.length);
+
+console.log()
+
+colors = ["red", "green", "blue"];
+console.log(colors);
+colors.push("purple");
+console.log.log(colors);
+
+colors.pop();
+console.log(colors);
+

6c985b1ce0aa4d82d364a3343b255e4ddfe3e654
Adderino <jonssonadam1@gmail.com>
2019-06-03 21:01:51 +0200

    ToDo klar

--- projects/ToDo/assets/js/todo.js
+++ projects/ToDo/assets/js/todo.js
@@ -1,24 +1,39 @@
-var todos = [];
-var btnActivate = document.getElementById("activate")
-btnActivate.addEventListener("click", function() {
-  var input = prompt("What would you like to do");
-  while(input !== "quit" || input !== "Quit") {
-    if(input === "new") {
-      var newTodo = prompt("what would you like to add");
-      todos.push(newTodo);
-      Console.log(newTodo + " was added to the list")
-    }
-    else if (input === "list"){
-      todos.forEach(function(todo, 1) {
-        console.log("On index" + 1 + ": "+ todo)
-      });
-    }
-    else if(input === "delete") {
-      var index = prompt("Enter index of todo to delete");
-      todos.splice(index, 1);
-      console.log("Which index would you like to delete?")
+document.getElementsByTagName("button")[0].addEventListener("click", List) //addEventListener makes the button start something amazing. An action will basically be performed
 
-    }
-    input = prompt("What would you like to do?")
-    }
-});
+var MyToDoList = []
+
+function List() {
+
+        var input = 0;
+     
+        while(input !== "exit" || input !== "Exit") { //As long as you don't write exit the code loops.
+
+            input = prompt("Vad vill du göra?");
+            if(input === "new" || input === "New") {
+                 var addNew = prompt("Write something to add to the list")
+                 MyToDoList.push(addNew); // Add a new thing you need to do.
+                 console.log("You added " + addNew + " to the list!")
+                        }
+            
+            
+             else if(input === "list" || input === "List") {
+                for(var i = 0; i <= MyToDoList.length-1; i++) //It logs index 0 and then adds 1 to the number which will be logged next, that way it runs through them all.
+                {
+                    console.log(MyToDoList[i])
+                }
+               
+             }  
+             
+             else if(input === "delete" || input === "Delete") {
+                    var indxDel = Number(prompt("Write a number to remove from the list"))
+                 
+                    MyToDoList.splice(indxDel, 1) // Remove a chosen item from the list
+                    console.log("you deleted index: " + indxDel)
+                 }
+                   
+             else if(input === "exit" || input === "Exit") // If you enter exit, you exit. simple as that... break breaks the loop.
+                break;
+        }
+        if(input === "exit" || input === "Exit") // Quit the prompt
+            console.log("Closing...")
+}
